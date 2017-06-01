850b674d8ea815b0ad29f8dde9b3bdbcc748d943
Diego <Diego@MacBook-Pro-de-Diego.local>
2017-06-01 19:08:59 -0300

    Atualizando o JPAUtil

--- src/main/java/DAO/JPAUtil.java
+++ src/main/java/DAO/JPAUtil.java
@@ -4,16 +4,32 @@ package DAO;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 public class JPAUtil {
 	
-private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Persistencia");
+	
+	@PersistenceContext(unitName = "Persistence")
+	protected EntityManager em;
+	
+	private EntityManager getEntityManager(){
+		return em;
+	}
+	
+	
+	
+	
+	
+	
+/////////////////////////////////////////
+	
+//private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Persistencia");
 
-public EntityManager geEntityManager(){
-	return emf.createEntityManager();
-}
+//public EntityManager geEntityManager(){
+	//return emf.createEntityManager();
+//}
 
-public void close(EntityManager em){
-	em.close();
-}
+//public void close(EntityManager em){
+	//em.close();
+//}
 }
